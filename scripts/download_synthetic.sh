48e0a0141e7827edd2ddbec3f9a6dce93dd36f6f
Hakam Shams <hakam.shams@hotmail.com>
2024-10-29 15:23:36 +0100

    Update download_synthetic.sh
    add a comment about download

--- scripts/download_synthetic.sh
+++ scripts/download_synthetic.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# you can download the data directly from [https://doi.org/10.60507/FK2/RD9E33]
 
 wget --continue  https://bonndata.uni-bonn.de/api/access/datafile/7506 -O Synthetic.7z
  
